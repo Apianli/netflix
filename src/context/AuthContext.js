679cf3125d7e7487c3c3b716303903e9a223c882
Ali Abbasi <web.designerr.94@gmail.com>
2022-05-19 09:59:09 +0430

    first commit

---
+++ src/context/AuthContext.js
@@ -0,0 +1,45 @@
+ import React, { createContext, useContext, useEffect, useState } from "react";
+ import { auth, db } from "../firebase";
+ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+ import { setDoc, doc } from "firebase/firestore";
+
+ const AuthContext = createContext()
+
+ export function AuthContextProvider ({children}) {
+     const [user, setUser] = useState({})
+
+     function signUp(email, password) {
+        createUserWithEmailAndPassword(auth, email, password);
+        setDoc(doc(db, 'users', email), {
+            savedMovies: []
+        })
+     }
+     function logIn(email, password) {
+        return signInWithEmailAndPassword(auth, email, password)
+     }
+
+     function logOut() {
+         return signOut(auth)
+     }
+
+     useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+        })
+        return () => {
+            unSubscribe();
+        }
+     },[])
+
+
+     return (
+         <AuthContext.Provider value={{ signUp, user, logIn, logOut }}>
+             {children}
+         </AuthContext.Provider>
+     )
+ }
+
+
+ export function UserAuth() {
+     return useContext(AuthContext)
+ }
